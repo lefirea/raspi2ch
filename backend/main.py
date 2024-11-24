14a0ccf7d9d40c20a85a5b0d1a9a1a2c90ab8c9f
urushihara <kosuke.urushihara@glucose.jp>
2024-11-24 20:53:17 +0900

    動作確認

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
-print("welcome to python")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def test():
+    return "welcome to Flask Page!"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="192.168.11.5")
